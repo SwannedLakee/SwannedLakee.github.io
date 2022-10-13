140b19fa9f1b5aeaaacf2998154bd7aceeb64cef
Ray Stetler <110113974+SwannedLakee@users.noreply.github.com>
2022-10-13 19:36:31 +0000

    Update build-site.el

--- build-site.el
+++ build-site.el
@@ -37,8 +37,7 @@
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :section-numbers nil       ;; Don't include section numbers)))
 
 ;; Generate the site output
 (org-publish-all t)
